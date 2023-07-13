09ebbf15c7e7843ecce8d707ff342f81a4568290
Andrey Khoryakov <a.khoryakov@ext.hightech.group>
2023-07-13 12:43:28 +0300

    [remove] logic to get request handler

--- SyncTelegramBot/SyncTelegramBot/Controllers/HomeController.cs
+++ SyncTelegramBot/SyncTelegramBot/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-using System.Reflection;
-using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
-using SyncTelegramBot.Models.Entities;
-using SyncTelegramBot.Models.HelpModels;
 using SyncTelegramBot.Services.Abstractions;
 
 namespace SyncTelegramBot.Controllers;
@@ -12,59 +8,22 @@ namespace SyncTelegramBot.Controllers;
 public partial class HomeController : Controller
 {
     private IUNFClient _unfClient;
+    private IGetRequestHandler _getRequestHandler;
 
-    public HomeController(IUNFClient unfClient)
+    public HomeController(IUNFClient unfClient, IGetRequestHandler getRequestHandler)
     {
         _unfClient = unfClient;
+        _getRequestHandler = getRequestHandler;
     }
     
     [HttpGet]
     public async Task<JsonResult> GetList(string filter)
     {
-        AnswerFromAPI res;
-        var ans = await _unfClient.GetFromUNF(filter);
-        var entityName = filter.Split(new[] { '/', '\\', '?' })[0];
-        object? output;
-        try
-        {
-            var entityType = types[entityName];
-            output = await ans.Content.ReadFromJsonAsync(entityType);
-            res = new() { Answer = output?.ToString()};
-        }
-        catch (KeyNotFoundException exception)
-        {
-            res = new()
-            {
-                Answer = "На данный момент для отображения этого списка неизвестны уникальные параменты"
-            };
-        }
-        catch
-        {
-            res = new()
-            {
-                Answer = "Произошла непредвиденная ошибка при обработке запроса"
-            };
-        }
-        
-     
-        return Json(res);
+        return Json(await _getRequestHandler.GetList(_unfClient, filter));
     }
-
 }
 
 public partial class HomeController
 {
-    private Dictionary<string, Type> types = new()
-    {
-        ["Catalog_СтавкиНДС"] = typeof(AnswerFromUNF<VATRate>),
-        ["Catalog_Сотрудники"] = typeof(AnswerFromUNF<Employee>),
-        ["Catalog_Организации"] = typeof(AnswerFromUNF<Organisation>),
-        ["Catalog_ВидыНалогов"] = typeof(AnswerFromUNF<TypesOfTaxes>),
-        ["Catalog_Контрагенты"] = typeof(AnswerFromUNF<Contragent>),
-        ["Catalog_БанковскиеСчета"] = typeof(AnswerFromUNF<BankAccount>),
-        ["ChartOfAccounts_Управленческий"] = typeof(AnswerFromUNF<Correspondence>),
-        ["Catalog_ДоговорыКонтрагентов"] = typeof(AnswerFromUNF<ContragentContract>),
-        ["Document_ПриходнаяНакладная"] = typeof(AnswerFromUNF<ReceiptInvoice>),
-        ["Document_ДоговорКредитаИЗайма"] = typeof(AnswerFromUNF<LoanAgreement>)
-    };
+ 
 }
