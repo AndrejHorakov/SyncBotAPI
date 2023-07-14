96374934b53be2fbc7afb46e6711e5f45261671a
Andrey Khoryakov <a.khoryakov@ext.hightech.group>
2023-07-14 18:23:06 +0300

    [add] method post moving

--- SyncTelegramBot/SyncTelegramBot/Controllers/HomeController.cs
+++ SyncTelegramBot/SyncTelegramBot/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using SyncTelegramBot.Models.HelpModels;
 using SyncTelegramBot.Services.Abstractions;
 
 namespace SyncTelegramBot.Controllers;
@@ -20,6 +22,15 @@ public partial class HomeController : Controller
     public async Task<JsonResult> GetList(string filter)
     {
         return Json(await _getRequestHandler.GetList(_unfClient, filter));
+    }
+
+    [HttpPost]
+    [Route("/Moving")]
+    public async Task<JsonResult> SaveMoving()
+    {
+        var res = new AnswerFromAPI();
+        HttpResponseMessage? ans = null;
+        return Json("");
     }
 }
 
